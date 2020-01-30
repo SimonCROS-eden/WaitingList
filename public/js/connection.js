5a452c96c2d908027f1fd09313c816a668cc35f5
SeaMoon15 <simon.dc.cros@gmail.com>
2020-01-30 21:27:42 +0100

    Remove unused require

--- public/js/connection.js
+++ public/js/connection.js
@@ -1,5 +1,7 @@
 let socket = io.connect('localhost:4000');
 
-socket.on('command', function(message) {
+socket.emit("test", {content: "culcul"});
 
+socket.on('reponseTest', function(message) {
+    console.log(message.content);
 });
