e9dfc896c95b1bb08f54bd32c02646d20db07419
SolalSTR <sstradella@gmail.com>
2020-02-05 16:11:15 +0100

    Apply stash

--- public/js/app.js
+++ public/js/app.js
@@ -50950,8 +50950,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/simoncros/exercices/WaitingList/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/simoncros/exercices/WaitingList/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/solalstradella/Technique/Sites/php/WaitingList/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/solalstradella/Technique/Sites/php/WaitingList/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
