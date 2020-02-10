894bdcdcb391b3438433b1b0633facdbe89426d0
Anthony.V <45999044+AnthonyVallenet@users.noreply.github.com>
2020-02-10 15:37:54 +0100

    Create ticket (#10)
    * Test resolve
    
    * Add file to gitignore
    
    * commit branch createTicket
    
    Co-authored-by: Simon CROS <45012357+SimonCROS@users.noreply.github.com>

---
+++ resources/views/ticket/create.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    @auth
+        <h1>Create</h1>
+        <form method="POST" action="/ticket">
+            @csrf
+
+            <input id="name" type="text" value="{{ old('name') }}" class="form-control @error('name') is-invalid @enderror" name="name" required autocomplete="name" placeholder="Titre ticket">
+            @error('name')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
+
+            <input id="ask_id" type="hidden" value="{{ Auth::user()->id }}" class="form-control @error('ask_id') is-invalid @enderror" name="ask_id" required>
+
+            <textarea id="desc" name="desc" class="form-control @error('desc') is-invalid @enderror" rows="5" cols="33" placeholder="Description">{{ old('desc') }}</textarea>
+            @error('desc')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
+
+            <button type="submit" class="btn btn-primary">
+                Create
+            </button>
+
+        </form>
+    @endauth
+</div>
+@endsection
