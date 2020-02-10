c0b7bb359c32ee6790f71e58a7378c0f447ee9a0
SimonCROS <scros@edenschool.fr>
2020-02-10 16:42:02 +0100

    Create factory and seeder

--- database/seeds/TicketSeeder.php
+++ database/seeds/TicketSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Ticket;
 
 class TicketSeeder extends Seeder
 {
@@ -11,6 +12,8 @@ class TicketSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Ticket::query()->delete();
+        DB::statement('SET FOREIGN_KEY_CHECKS=0');
+        factory(Ticket::class, 10)->create();
     }
 }
