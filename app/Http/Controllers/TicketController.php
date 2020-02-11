4fbae29f2e8b1c3602d31a3b829f419c2d7fe9b2
SolalS <46782740+SolalSTR@users.noreply.github.com>
2020-02-11 11:30:48 +0100

    Delete ticket (#17)
    * Test resolve
    
    * Add file to gitignore
    
    * delete ticket working
    
    * admin can delete
    
    Co-authored-by: Simon CROS <45012357+SimonCROS@users.noreply.github.com>

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -99,8 +99,10 @@ class TicketController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Ticket $ticket)
     {
-        //
+        $ticket->delete();
+
+        return redirect('/');
     }
 }
--- resources/views/ticket/show.blade.php
+++ resources/views/ticket/show.blade.php
@@ -7,6 +7,17 @@
         <p>{{ $ticket->asker->first_name }} {{ $ticket->asker->last_name }}</p>
         <h2>{{ $ticket->name }}</h2>
         <p>{{ $ticket->desc }}</p>
+
+        @if ($ticket->asker == Auth::user() || Auth::user()->isAdmin())
+            <form action="{{ $ticket->id }}" method="post">
+                {{ method_field('DELETE') }}
+                @csrf
+                <button type="submit">
+                    Supprimer
+                </button>
+
+            </form>
+        @endif
     @endauth
 </div>
 @endsection
