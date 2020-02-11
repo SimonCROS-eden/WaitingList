3e18fd2abbfdff9b0bd3602f5b48a3ad71943388
SolalS <46782740+SolalSTR@users.noreply.github.com>
2020-02-11 09:48:12 +0100

    Show ticket (#16)
    * Test resolve
    
    * Add file to gitignore
    
    * Pull commit
    
    Co-authored-by: Simon CROS <45012357+SimonCROS@users.noreply.github.com>

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,8 +4,5 @@
 <div class="container">
     <button id="test">Notifier</button>
     <p>T'es sur le dashboard</p>
-    @foreach($tickets as $ticket)
-        <a href="/ticket/{{ $ticket->id }}"><p>{{ $ticket->name }}</p></a>
-    @endforeach
 </div>
 @endsection
