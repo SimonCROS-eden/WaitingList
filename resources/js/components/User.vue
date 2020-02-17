c18d74f7782c2917d58f8ca7518fd1dcc1f07914
Simon CROS <45012357+SimonCROS@users.noreply.github.com>
2020-02-17 14:08:08 +0100

    Dashboard style (#34)
    * start style
    
    * Before merge
    
    * Style started
    
    * Style started

---
+++ resources/js/components/User.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="d-flex align-items-center justify-content-between">
+        <span class="mr-2">{{ name }}</span>
+        <div class="h5 m-0 d-flex no-wrap">
+            <span :class="'badge ' + (score >= 0 ? 'badge-success' : 'badge-danger')"><span class="mr-2">↑↓</span>{{ score }}</span> 
+            <span v-if="admin" class="ml-2 badge badge-primary"><span class="mr-2">?</span>{{ ask }}</span> 
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: [
+            'name',
+            'score',
+            'ask',
+            'admin'
+        ]
+    }
+</script>
