005e42afa50ebfd8541236930b9eeb080f8cdc47
Anthony Vallenet <anthonyvallenet@MacBook-Pro-Anthony.local>
2020-02-18 14:34:20 +0100

    commit style page tag

--- resources/views/tag.blade.php
+++ resources/views/tag.blade.php
@@ -2,56 +2,62 @@
 
 @section('content')
 <div class="container">
-    <div>
-        <h1>Tags</h1>
-        <form method="POST" action="/tag">
-            @csrf
+    <div class="pt-5">
+        <div class="d-flex justify-content-between mb-5">
+            <h1 class="">Tags</h1>
+            <div class="d-flex align-items-center">
+                <form class="d-flex align-items-center" method="POST" action="/tag">
+                    @csrf
 
-            <input id="name" type="text" value="{{ old('name') }}" class="form-control @error('name') is-invalid @enderror" name="name" required autocomplete="name" placeholder="Titre name">
-            @error('name')
-                <span class="invalid-feedback" role="alert">
-                    <strong>{{ $message }}</strong>
-                </span>
-            @enderror
+                    <input id="name" type="text" value="{{ old('name') }}" class="form-control @error('name') is-invalid @enderror" name="name" required autocomplete="name" placeholder="Nom tag">
+                    @error('name')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                    
+                    <input id="color" type="color" value="{{ old('color') }}" class="input-button @error('color') is-invalid @enderror" name="color" required>
+                    @error('color')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+
+                    <button type="submit" class="btn btn-primary">
+                        Create
+                    </button>
+                </form>
+            </div>
             
-            <input id="color" type="color" value="{{ old('color') }}" class="form-control @error('color') is-invalid @enderror" name="color" required>
-            @error('color')
-                <span class="invalid-feedback" role="alert">
-                    <strong>{{ $message }}</strong>
-                </span>
-            @enderror
-            
-
-            <button type="submit" class="btn btn-primary">
-                Create
-            </button>
-
-        </form>
+        </div>
         
-        <ul>
+        <div>
             @foreach ($tags as $tag)
-                <li style="color: {{$tag->color}}">
-                    <p class="tags" style="color: {{ $tag->color }}">{{ $tag->name }}</p>
-                    <div style="display:none">
-                        <form action="/tag/{{ $tag->id }}" method="post">
+            <div class="d-inline">
+                <p class="tags mb-2 mr-1 px-3 py-2 badge text text-white" style="background-color: {{ $tag->color }}">{{ $tag->name }}</p>
+                <div class="mr-1 mb-2 px-3 py-1 badge text-white" style="display:none; border: solid 1px {{ $tag->color }}">
+                    <div class="d-flex align-items-center">
+                        <form class="d-flex mr-2" action="/tag/{{ $tag->id }}" method="post">
                             @csrf
                             @method('PUT')
-                            <input id="{{ $tag->name }}" type="text" value="{{ $tag->name }}" class="form-control @error('name') is-invalid @enderror" name="{{ $tag->name }}" required autocomplete="name">
+                            <input id="{{ $tag->name }}" type="text" value="{{ $tag->name }}" class="border-0 text @error('name') is-invalid @enderror" name="{{ $tag->name }}" required autocomplete="name">
 
-                            <input id="{{ $tag->color }}" type="color" value="{{ old('color') ? old('color') : $tag->color }}" class="form-control @error('color') is-invalid @enderror" name="{{ $tag->color }}" required>
+                            <input class="input-button" id="{{ $tag->color }}" type="color" value="{{ old('color') ? old('color') : $tag->color }}" class="@error('color') is-invalid @enderror" name="{{ $tag->color }}" required>
 
-                            <button type="submit">ok</button>
+                            <button class="input-button" style="color: {{ $tag->color }}" type="submit">ok</button>
                         </form>
                         <form action="/tag/{{ $tag->id }}" method="post">
                             @csrf
                             @method('DELETE')
-                            <button type="submit">delete</button>
+                            <button class="input-button text-danger" type="submit">delete</button>
                         </form>
                     </div>
-                    
-                </li>
+                </div>
+            </div>
+                
             @endforeach
-        </ul>
+        </div>
+        
     </div>
 </div>
 @endsection
